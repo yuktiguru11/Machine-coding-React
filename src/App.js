42b7cb3f94e86a1b3c023bedbc67d35190f4aacb
Yukti Guru <yuktiguru@gmail.com>
2024-01-15 22:59:49 +0530

    form and api changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-
+import {useState} from 'react';
 
 function App() {
+  const [name,setName] = useState('')
+  const [mail,setEmail] = useState('')
+  const [pass,setPass] = useState('')
+
+  async function registerUser(event){
+    event.preventDefault();
+    const response = await fetch('http://localhost:3000/api/register',{
+      method:'POST',
+      header: {
+        'Content-Type' : 'application-json'
+      },
+      body: JSON.stringify({
+        name,
+       mail,
+        pass
+      }),
+    })
+
+    const data = await  response.json();
+    console.log(data);
+  }
   return (
     <div>
-      
+      Register
+      <form onSubmit={registerUser}>
+      <input value = {name} 
+      onChange={(e)=> setName(e.target.value)}
+      placeholder ="Name" type="text"/>
+      <br/>
+      <input value = {mail} 
+      onChange={(e)=> setEmail(e.target.value)} placeholder ="Email" type="email"/>
+      <br/>
+      <input value = {pass}
+      onChange={(e)=> setPass(e.target.value)}
+       placeholder ="password" type="password"/>
+       <br/>
+       <input type="submit" placeholder='Register'/>
+      </form>
     </div>
   );
 }
