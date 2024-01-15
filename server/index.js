921ba2021e26ac3ac93df8e917f0c44399019c13
Yukti Guru <yuktiguru@gmail.com>
2024-01-15 23:33:16 +0530

    server  changes

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors())
+app.use(express.json())
+
+app.post('/api/register',(req,res)=>{
+    console.log(req.body)
+    res.json({status : 'ok'})
+
+});
+
+app.listen(3000, ()=>{
+    console.log("listening at 3000")
+})
+
