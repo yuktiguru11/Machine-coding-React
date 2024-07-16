8026d9e820ddcb2301ec6dbe2366b565e38fb034
Yukti Guru <yuktiguru@gmail.com>
2024-07-16 22:50:28 +0530

    fetching product details api

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,4 +1,20 @@
+import { useEffect, useReducer} from 'react'
+import axios from "axios"
+
 function App() {
+
+  const [state , dispatch] = useReducer()
+
+  const getProductDetails = async()=>{
+    const data = await axios.get('https://dummyjson.com/products');
+    const productDetails =  data.data.products;
+    console.log(productDetails)
+  }
+
+  useEffect(()=>{
+    getProductDetails();
+  },[])
+
   return (
     <div >
       
