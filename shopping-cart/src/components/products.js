2ad96520a5cdacffab303b16888be2c480436b04
Yukti Guru <yuktiguru@gmail.com>
2024-07-17 00:18:53 +0530

    design changes

--- shopping-cart/src/components/products.js
+++ shopping-cart/src/components/products.js
@@ -1,41 +1,40 @@
+const Products = ({ state, dispatch }) => {
+  const { products, cart } = state;
 
-
-const Products = ({state, dispatch})=> {
-
-    const {products, cart} = state
-
-    return (
-        <>
-        <div
-        style ={{
-            display: "flex",
-            felxWrap: "wrap",
-            justifyContent: "space-evenly",
-            width:"80%"
+  return (
+    <>
+      <div
+        className="App"
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-evenly",
+          width: "80%",
         }}
-        >
-            {products.map((prod)=>(
-                <div
-                key ={prod.id}
-                style={{
-                    display:"flex",
-                    flexDirection:"column",
-                    padding: 10,
-                    border: "1px solid grey",
-                    width : "30%",
-                    marginTop: 10,
-                    gap : 10
-                }}>
-                    <img src= {prod.thumbnail}
-                    alt={prod.title}
-                    style={{height: 200, objectFit:"cover"}}></img>
+      >
+        {products.map((prod) => (
+          <div
+            key={prod.id}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: 10,
+              border: "1px solid grey",
+              width: "30%",
+              marginTop: 10,
+              gap: 10,
+            }}
+          >
+            <img
+              src={prod.thumbnail}
+              alt={prod.title}
+              style={{ height: 200, objectFit: "cover" }}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-                </div>
-            ))}
-        
-        </div>
-        </>
-    )
-}
-
-export default Products
+export default Products;
