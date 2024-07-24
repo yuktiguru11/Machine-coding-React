65ad25679d0aa719e9224d6891c7a73eb2afa8b8
Yukti Guru <yuktiguru@gmail.com>
2024-07-24 18:38:39 +0530

    Adding different pages

---
+++ dark-light-mode/pages/blog.jsx
@@ -0,0 +1,8 @@
+const Blog = ()=>{
+    return (
+        <>
+        <h1>Blog Page</h1>
+        </>
+    )
+}
+export default Blog
--- dark-light-mode/src/App.js
+++ dark-light-mode/src/App.js
@@ -1,9 +1,11 @@
 function App() {
-  return (
-    <div >
-      
-    </div>
-  );
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/about" element={<About/>}></Route>
+    <Route path="/blog" element={<Blog/>}></Route>
+  </Routes>
+  </BrowserRouter>
 }
 
 export default App;
