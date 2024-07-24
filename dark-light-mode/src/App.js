94bd49b90a4e7e38570c49aae8b344877612fb4a
Yukti Guru <yuktiguru@gmail.com>
2024-07-24 19:12:28 +0530

    path addition

--- dark-light-mode/src/App.js
+++ dark-light-mode/src/App.js
@@ -1,3 +1,8 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from "./pages/home.jsx";
+import About from "./pages/about.jsx";
+import Blog from "./pages/blog.jsx";
+
 function App() {
   return <BrowserRouter>
   <Routes>
