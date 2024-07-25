ac75d2a41112accd2654c020c03ed1c9fd2effb2
Yukti Guru <yuktiguru@gmail.com>
2024-07-25 12:38:17 +0530

    pages theme changes

--- dark-light-mode/src/pages/blog.jsx
+++ dark-light-mode/src/pages/blog.jsx
@@ -1,6 +1,10 @@
+
+import { useThemeContext } from "../theme-context";
 const Blog = ()=>{
+    const { theme } = useThemeContext();
     return (
-        <div className="page">
+        
+    <div className={`page ${theme}`}>
         <h1>Blog Page</h1>
         <p>Read our latest blog posts!</p>
         </div>
--- dark-light-mode/src/pages/home.jsx
+++ dark-light-mode/src/pages/home.jsx
@@ -1,6 +1,9 @@
+import { useThemeContext } from "../theme-context";
 const Home = ()=>{
+    const { theme } = useThemeContext();
     return (
-        <div className="page">
+        
+    <div className={`page ${theme}`}>
         <h1>Home Page</h1>
         <p>Read our latest homme posts!</p>
         </div>
