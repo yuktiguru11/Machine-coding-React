4dfbffa0fd37864aba2c39cac84a2dde9fdd9f8b
Yukti Guru <yuktiguru@gmail.com>
2024-07-25 12:37:56 +0530

    navbar styling changes

--- dark-light-mode/src/components/navbar.jsx
+++ dark-light-mode/src/components/navbar.jsx
@@ -1,13 +1,28 @@
 import { Link} from "react-router-dom"
+import {useThemeContext} from "../theme-context"
 
 const Navbar =()=>{
+    const {theme, themeToggle} = useThemeContext();
+
+    const toggleMode = ()=>{
+        themeToggle()
+    }
     return (
         <>
-        <nav className= "navbar">
+        <nav className= {`navbar ${theme}`}>
             <div>
                 <Link to = "/">Home</Link>
                 <Link to = "/about">About</Link>
                 <Link to = "/blog">Blog</Link>
+            </div>
+            <div className = "mode-switch">
+                <label>
+                    <input type="checkbox"
+                    onChange={toggleMode}
+                    checked = {theme === "dark"}>
+                    </input>
+                    <span className="slider round"></span>
+                </label>
             </div>
         </nav>
         </>
