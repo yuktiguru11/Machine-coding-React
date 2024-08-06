f9840393930b3d224d2c8d22afc2dd150cac5dd8
Yukti Guru <yuktiguru@gmail.com>
2024-08-06 19:51:26 +0530

    fetching the product data

--- breadcrumds/src/App.js
+++ breadcrumds/src/App.js
@@ -1,6 +1,18 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { useEffect } from "react";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import ProductsList from "./pages/ProductsList";
 
 function App() {
+
+  useEffect(()=>{
+    fetch('https://dummyjson.com/products').then(res => res.json())
+    .then((result)=>{
+      console.log(result)
+    })
+  },[])
+
   return (
     <BrowserRouter>
     <div className="App">
