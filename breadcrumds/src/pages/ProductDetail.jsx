6b38f7d3e073a0f664923fcff78c9e3e9e8e8ccd
Yukti Guru <yuktiguru@gmail.com>
2024-08-06 21:05:39 +0530

    product details page changes

--- breadcrumds/src/pages/ProductDetail.jsx
+++ breadcrumds/src/pages/ProductDetail.jsx
@@ -1,6 +1,30 @@
+import { useState , useEffect} from "react";
+import { useParams } from "react-router-dom"
+
 const ProductDetail = () => {
+const {id} = useParams();
+const [product, setProduct] = useState(null)
+
+useEffect(()=>{
+    fetch(`https://dummyjson.com/products/${id}`).then(res => res.json())
+    .then((result)=>{
+      setProduct(result)
+    })
+  },[])
+
     return(
-        <h1>Product Details</h1>
+        {product:(
+            <div style={{display: "flex"}}>
+                <img style = {{height: 300}}src={product.thumbnail}></img>
+                <div>
+                <h3>{product.title}</h3>
+                <h3>{product.price}</h3>
+                <h3>{product.description}</h3>
+                </div>
+                </div>
+            
+        )}
+        
     )
 }
 
