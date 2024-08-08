048ebd89e4b6495561f2ddb32b2aa224ca392ed7
Yukti Guru <yuktiguru@gmail.com>
2024-08-08 17:19:05 +0530

    initial setup for otp login

--- password-generator/src/App.js
+++ password-generator/src/App.js
@@ -1,9 +1,10 @@
-
+import OtpLogin from "./component/OtpLogin";
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Login with Phone</h1>
+      <OtpLogin/>
     </div>
   );
 }
---
+++ password-generator/src/component/OtpLogin.jsx
@@ -0,0 +1,7 @@
+const OtpLogin = ()=>{
+    return (
+        <></>
+    )
+}
+
+export default OtpLogin
