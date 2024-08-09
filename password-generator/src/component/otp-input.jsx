728e23f7c26dd514001e48e7cfde3520d48dfa96
Yukti Guru <yuktiguru@gmail.com>
2024-08-09 19:01:43 +0530

    otp-input chnages

--- password-generator/src/component/otp-input.jsx
+++ password-generator/src/component/otp-input.jsx
@@ -1,10 +1,21 @@
-import {useState} from "react"
+import {useState, useRef, useEffect} from "react"
 
 const OtpInput = ({length = 4, onOtpSubmit = ()=>{}})=>{
     const [otp, setOtp] = useState(new Array(length).fill(""));
+    const currentRef = useRef([]);
 
-    const handleChange =()=>{
+    useEffect(()=>{
+        if(currentRef.current[0]){
+        currentRef.current[0].focus();}
+    },[])
 
+    const handleChange =(index, e)=>{
+        const value = e.target.value;
+        if(isNaN(value)) return ;
+
+        const newOtp = [...otp];
+        newOtp[index]=value.substring(value.length-1);
+        setOtp(newOtp);
     }
 
     const handleClick =()=>{
@@ -18,7 +29,8 @@ const OtpInput = ({length = 4, onOtpSubmit = ()=>{}})=>{
     return (
         <>
         {otp.map((value, index)=>(
-            <input type="text" key={index} value={value}
+            <input
+            ref={(input)=>(currentRef.current[index] = input)} type="text" key={index} value={value}
             onChange={(event)=>handleChange(index,event)}
             onClick = {()=>handleClick(index) }
             onKeyDown={()=>handleKeyDown(index)} className="otpInput">
