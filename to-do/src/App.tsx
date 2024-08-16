aa1942b0f8e71402855475d36ca5aa7b757426e5
Yukti Guru <yuktiguru@gmail.com>
2024-08-16 21:02:04 +0530

    input addition

--- to-do/src/App.tsx
+++ to-do/src/App.tsx
@@ -1,9 +1,25 @@
-
+import { useState } from "react";
 
 function App() {
+  const [input, setInput ] = useState<string >("")
+  const array : string[] = []
+
+  const handleOnChange =(e : React.ChangeEvent<HTMLInputElement>)=>{
+    setInput(e.target.value)
+  }
+
+  const handleSubmit = (e : React.MouseEvent<HTMLButtonElement>)=>{
+    array.push(input)
+  }
   return (
     <div>
-      
+      <input type="text" onChange={handleOnChange} />
+      <button onClick={handleSubmit}>Submit</button>
+      {array.map((arr)=>(
+        <div>{arr}</div>
+            
+         
+      ))}
     </div>
   );
 }
