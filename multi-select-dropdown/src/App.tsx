f704193364c49dddbcfef67b43b9d5f7eb08c935
Yukti Guru <yuktiguru@gmail.com>
2024-08-19 12:36:32 +0530

    get call fix

--- multi-select-dropdown/src/App.tsx
+++ multi-select-dropdown/src/App.tsx
@@ -5,11 +5,14 @@ interface User {
   firstName: string;
   lastName : string; 
 }
+interface ServerResponse {
+  data : User
+}
 
 
 function App() {
   const [searchInput, setSearchInput] = useState<string>("");
-  const [searchSuggestion, setSearchSuggestion] = useState<string[]>([]);
+  const [searchSuggestion, setSearchSuggestion] = useState<User[]>([]);
 
   const handleChange =(e : React.ChangeEvent<HTMLInputElement>)=>{
     setSearchInput(e.target.value);
@@ -18,8 +21,20 @@ function App() {
   let ep = 'https://dummyjson.com/users/search?q='
 
   useEffect(()=>{
-    axios.get<User[]>(ep).then((res: AxiosResponse)=>{
-
+    if(searchInput.trim()==""){
+      setSearchSuggestion([]);
+      return;
+    }
+    // axios.get<User[]>(ep + searchInput).then((res: User[])=>{
+    //   setSearchSuggestion(res)
+    // })
+    axios.request<ServerResponse>({
+      url: 'https://example.com/path/to/data',
+      transformResponse: (r: ServerResponse) => r.data
+    }).then((response) => {
+      // `response` is of type `AxiosResponse<ServerData>`
+      const { data } = response
+      // `data` is of type ServerData, correctly inferred
     })
   },[searchInput])
 
